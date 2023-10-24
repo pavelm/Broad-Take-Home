97302dac163fd7df82e2804a337434e0c084ea6e
evansuslovich <evanjacobsuslovich@gmail.com>
2023-10-24 09:04:18 -0400

    renamed variables, created fetch_data courontine for aPI requests (easier readability)

--- MBTAStopsFetcher.py
+++ MBTAStopsFetcher.py
@@ -3,9 +3,10 @@ from enum import IntEnum
 import asyncio
 import aiohttp
 
+# Set up logging
 logging.basicConfig(format="%(levelname)s - %(message)s", level=logging.INFO)
 
-# create an enumeration for RailType
+# Create an enumeration for RailType
 class RailType(IntEnum):
     LIGHT = 0
     HEAVY = 1
@@ -15,44 +16,32 @@ class MBTAStopsFetcher:
         self.api_base_url = api_base_url
         self.api_key = api_key
 
+    # Create a coroutine to fetch data from the API
+    async def fetch_data(self, session, subway_id):
+        url = f"{self.api_base_url}/stops?filter[route]={subway_id}"
+        headers = {"x-api-key": self.api_key}
+        async with session.get(url, headers=headers, ssl=False) as response:
+            return await response.json()
 
-    # Coroutines are computer program components that allow execution
-    # to be suspended and resumed, generalizing subroutines for cooperative multitasking
-    # https://en.wikipedia.org/wiki/Coroutine
-    # we can get a set of all scheduled and running tasks
+    # Get a list of tasks for fetching data
     def get_tasks(self, session, list_of_subway_route_ids):
-        tasks = []
-        for subway_id in list_of_subway_route_ids:
-            tasks.append(asyncio.create_task(
-                session.get(f'{self.api_base_url}/stops?filter[route]=' + str(subway_id),
-                        headers={"x-api-key": self.api_key}, ssl=False)))
-        return tasks
+        return [self.fetch_data(session, subway_id) for subway_id in list_of_subway_route_ids]
 
-
-    # gets a dictionary of routes and their corresponding stops
-    # https://www.youtube.com/watch?v=nFn4_nA_yk8
-    # https://groups.google.com/g/massdotdevelopers/c/WiJUyGIpHdI
+    # Get a dictionary of routes and their corresponding stops
     async def get_dict_of_routes_and_stops(self, list_of_subway_route_ids, mbta_routes_long_names):
         async with aiohttp.ClientSession() as session:
-
             tasks = self.get_tasks(session, list_of_subway_route_ids)
             data = await asyncio.gather(*tasks)
 
-            # create an empty dictionary
             route_stop_dict = {}
 
-            for index in range(len(data)):
-                # get teh subway line data
-                subway_line_data = await (data[index]).json()
-
-                subway_line_data = subway_line_data['data']
-
-                route_stop_dict[mbta_routes_long_names[index]] = []
-
-                for stops in subway_line_data:
-                    route_stop_dict[mbta_routes_long_names[index]].append((stops['attributes']['name']))
+            for index, subway_id in enumerate(list_of_subway_route_ids):
+                subway_line_data = data[index]["data"]
+                route_name = mbta_routes_long_names[index]
+                stops = [stop["attributes"]["name"] for stop in subway_line_data]
+                route_stop_dict[route_name] = stops
 
             return route_stop_dict
-        
+
     def run_get_dict_of_routes_and_stops(self, list_of_subway_route_ids, mbta_routes_long_names):
         return asyncio.run(self.get_dict_of_routes_and_stops(list_of_subway_route_ids, mbta_routes_long_names))
